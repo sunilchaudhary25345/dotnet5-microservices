b0dbe26117780cbbf8b4ef20d647c41ba6780d35
Sunil Chaudhary <sunilchaudhary2304@gmail.com>
2021-12-30 20:36:42 -0600

    Implemented Consume logic on order microservices using MassTransit and RabbitMQ

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,34 @@
+﻿
+using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMediator mediator;
+        private readonly IMapper mapper;
+        private readonly ILogger<BasketCheckoutConsumer> logger;
+
+        public BasketCheckoutConsumer(IMediator mediator, IMapper mapper, ILogger<BasketCheckoutConsumer> logger)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await mediator.Send(command);
+
+            logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
+        }
+    }
+}
